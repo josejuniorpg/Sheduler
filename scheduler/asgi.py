d91f83a726b09d0eee57833318a44de34b6371f0
josejuniorpg <yunipgx@protonmail.com>
2023-09-20 19:06:33 -0600

    Configure the .env in asgo and wsgi files

--- scheduler/asgi.py
+++ scheduler/asgi.py
@@ -10,6 +10,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheduler.settings')
+# Packages imports
+import environ
+
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheduler.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', env.str('DJANGO_ENVIRONMENT_FILE', default='blog.settings.prod'))
 
 application = get_asgi_application()
--- scheduler/wsgi.py
+++ scheduler/wsgi.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheduler.settings')
+# Packages imports
+import environ
+
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheduler.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', env.str('DJANGO_ENVIRONMENT_FILE', default='blog.settings.prod'))
 
 application = get_wsgi_application()
