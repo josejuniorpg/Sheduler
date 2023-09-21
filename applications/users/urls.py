af2dcd1d456806f26912bb2b420dfa0dbb609b8f
josejuniorpg <yunipgx@protonmail.com>
2023-09-21 15:30:45 -0600

    Add urls file UsersApp.

---
+++ applications/users/urls.py
@@ -0,0 +1,11 @@
+# Django Imports
+from django.urls import path, include
+
+# Local Imports
+from . import views
+
+app_name = 'users_app'
+
+urlpatterns = [
+    # path('', views.NewDepartamentoView.as_view(), name='new-departamento'),
+]
