2bed1b9bcd57b404833505a87fada2ae4e3293cc
josejuniorpg <yunipgx@protonmail.com>
2023-09-21 16:12:20 -0600

    Tittle, lenguages add.

--- applications/home/views.py
+++ applications/home/views.py
@@ -1,7 +1,9 @@
 # Django Imports
 from django.shortcuts import render
 from django.views.generic import (TemplateView)
-# Create your views here.
+from django.utils.translation import gettext as _
 
 class HomePageView(TemplateView):
     template_name = "home/index.html"
+    output = _("Welcome | Home Page")
+    extra_context = {'title':output}
