2dcb3a5162790de2cb9d975dc716c75e5d6b96ce
josejuniorpg <yunipgx@protonmail.com>
2023-09-22 01:57:39 -0600

    The index path, and changes in footte.hmtl

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -7,7 +7,6 @@ from . import views
 app_name = 'home_app'
 
 urlpatterns = [
-    # path('', views.NewDepartamentoView.as_view(), name='new-departamento'),
-    path('home/', views.HomePageView.as_view(), name='home'),
+    path('', views.HomePageView.as_view(), name='home'),
     path('change-language/<str:new_language>/', views.CambiarIdiomaView.as_view(), name='change-language'),
 ]
