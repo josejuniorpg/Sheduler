39b3444edbc84a4c049eaabe6dc1ab2e7064e459
josejuniorpg <yunipgx@protonmail.com>
2023-10-12 22:10:42 -0600

    Traductions

--- applications/users/functions.py
+++ applications/users/functions.py
@@ -16,12 +16,12 @@ def code_generator(size=6, chars=string.ascii_uppercase + string.digits):
 
 def send_email_verify_code_by_user_id(id=None):
     user = get_object_or_404(User, id=id)
-    subject = _('Verification Code') + '  ' + _('For') + '  ' + user.first_name
+    subject = _('Verification Code') + '  ' + _('ForEmail') + '  ' + user.first_name
     message = (
-        f'{_("Verification Code")}:  {user.code_verification} \n{_("For")} {user.first_name}\n'
+        f'{_("Verification Code")}:  {user.code_verification} \n{_("ForEmail")} {user.first_name}\n'
         f'{_("Link")} {settings.LINK_BASE_URL}verification-user/{user.id}'
     )
     email_sender = settings.EMAIL_HOST_USER
-    send_mail(subject, message, email_sender, ['yunipgdes@gmail.com'])
+    send_mail(subject, message, email_sender, ['yunipgdes@gmail.com']) #todo Change for the user Email
     # send_mail(subject, message, from_email, [form.cleaned_data['email'], ])
 
