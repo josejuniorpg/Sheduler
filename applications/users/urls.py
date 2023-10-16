e42fa69b1cabca1712726f160d17c0a283751c1a
josejuniorpg <yunipgx@protonmail.com>
2023-10-16 02:54:09 -0600

    Logut View, and Login in Login and Logout Header Butons

--- applications/users/urls.py
+++ applications/users/urls.py
@@ -1,5 +1,6 @@
 # Django Imports
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 
 # Local Imports
 from . import views
@@ -9,6 +10,7 @@ app_name = 'users_app'
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('verification-user/<pk>/', views.CodeVerificationView.as_view(), name='verification-user'),
     path('send-again-email/<pk>/', views.send_again_email_view, name='send-again-email'),
 ]
