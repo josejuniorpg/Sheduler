f600e90db00a4fd1bd3b5fb25b53d1cca559fab3
josejuniorpg <yunipgx@protonmail.com>
2023-11-13 14:03:24 -0600

    Shift Add A other Button

--- applications/shifts/views.py
+++ applications/shifts/views.py
@@ -1,7 +1,7 @@
 # Django imports
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from applications.shifts.forms import ShiftForm
 from applications.shifts.models import Shift
@@ -14,5 +14,20 @@ class ShiftCreateView(CreateView):
     model = Shift
     form_class = ShiftForm
     success_url = reverse_lazy('home_app:home')
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            if 'save' in request.POST:
+                return redirect(reverse_lazy('home_app:home'))
+            elif 'save_and_add_another' in request.POST:
+                return redirect(reverse_lazy('shifts_app:create_shifts'))
+        # If the form is invalid, it renders the template with errors.
+        return render(request, self.template_name, {'form': form})
 
 
