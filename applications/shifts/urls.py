9bbca1ac4ef200056e61f4dc507472ba61c77081
josejuniorpg <yunipgx@protonmail.com>
2023-12-04 00:43:14 -0600

    Basic Search.

--- applications/shifts/urls.py
+++ applications/shifts/urls.py
@@ -7,6 +7,7 @@ from . import views
 app_name = 'shifts_app'
 
 urlpatterns = [
-    path('create-shift/', views.ShiftCreateView.as_view(), name='create_shifts'),
-    path('list-shift/', views.ShiftListView.as_view(), name='create_shifts'),
+    path('create-shift/', views.ShiftCreateView.as_view(), name='create-shifts'),
+    path('list-shift/', views.ShiftListView.as_view(), name='list-shifts'),
+    path('update-shift/', views.ShiftListView.as_view(), name='update-shifts'), #todo finish this template.
 ]
--- applications/shifts/views.py
+++ applications/shifts/views.py
@@ -1,7 +1,8 @@
 # Django imports
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 
 from applications.shifts.forms import ShiftForm
 from applications.shifts.models import Shift
@@ -26,14 +27,25 @@ class ShiftCreateView(CreateView):
             if 'save' in request.POST:
                 return redirect(reverse_lazy('home_app:home'))
             elif 'save_and_add_another' in request.POST:
-                return redirect(reverse_lazy('shifts_app:create_shifts'))
+                return redirect(reverse_lazy('shifts_app:create-shifts'))
         # If the form is invalid, it renders the template with errors.
         return render(request, self.template_name, {'form': form})
 
 
 class ShiftListView(ListView):
     template_name = "shifts/list_shifts.html"
-    model = Shift
+    # model = Shift
     context_object_name = 'shifts'
     paginate_by = 10
 
+    def get_queryset(self):
+        kwargs = self.request.GET.get('search-shifts', '')
+        print(kwargs)
+        queryset = Shift.objects.filter(
+            Q(user__first_name__icontains=kwargs) |
+            Q(user__last_name__icontains=kwargs))
+        return queryset
+
+
+class ShiftDetailsView(UpdateView):
+    template_name = "shifts/update_shifts.html"
